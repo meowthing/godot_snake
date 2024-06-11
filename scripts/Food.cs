c642d033fba9c8a19c5331ebe1b2fc1edd42f733
Vinnie Leung <vleung20@gmail.com>
2024-06-11 16:55:07 +0100

    changed food color

--- scripts/Food.cs
+++ scripts/Food.cs
@@ -4,7 +4,7 @@ public class Food
 {
     public Vector2 position;
     public Vector2 size = Grid.CellSize;
-    public Color color = Colors.LightPink;
+    public Color color = Colors.HotPink;
 
     public Rect2 GetRectangle()
     {
